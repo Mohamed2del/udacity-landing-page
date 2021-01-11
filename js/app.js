402ea2c3b02bf09a9bc601354d8935fa4d746e50
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-01-11 11:35:26 +0200

    onclick on js file

--- js/app.js
+++ js/app.js
@@ -97,6 +97,8 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+mybutton.addEventListener('click', topFunction);
 /**
  * End Helper Functions
  * Begin Main Functions
